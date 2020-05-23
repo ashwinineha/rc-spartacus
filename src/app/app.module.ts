70a9ba0d054cad480d1f01ac64b47c77a26bc943
Singh, Ashwini <ashwini.singh@sap.com>
2020-05-23 23:14:29 +1000

    first spartacus commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,71 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { translationChunksConfig, translations } from '@spartacus/assets';
+import { ConfigModule } from '@spartacus/core';
+import { B2cStorefrontModule } from '@spartacus/storefront';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'spartacus-app' }),
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://dev-com-17.accdemo.b2c.ydev.hybris.com:9002/',
+        },
+      },
+      context: {
+        baseSite: ['electronics-spa'],
+      },
+    }),
+
+    // demonstrate i18n
+    ConfigModule.withConfig({
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en',
+      },
+    }),
+
+    // CustomBannerModule,
+
+    // BootcampWebComponentModule,
+
+    // BootcampRoutingModule,
+
+    // // Module to configure the site context with multi-site features
+    // BootcampMultiSiteModule,
+    // // Demonstrate a custom context
+    // CatalogContextModule,
+
+    // BootcampSeoModule,
+
+    // BootcampStateModule,
+
+    // BootcampDataBindingModule,
+
+    // Demonstrate static CMS config
+    // BootcampStaticCmsModule,
+
+    // BootcampConfigModule,
+    // // Provide a custom them to the `ThemeConfig`.
+    // ConfigModule.withConfig({ theme: Theme.STRAWBERRIES } as ThemeConfig),
+
+    // // Demonstrate outlets. Don't forget to enable the `<app-outlets></app-outlets>` in the app.component.html
+    // BootcampOutletModule,
+
+    // BootcampIconModule,
+
+    // BootcampI18nModule,
+
+    // demonstrates layout configuration
+    // BootcampLayoutConfigModule,
+
+    // BootcampComponentModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
